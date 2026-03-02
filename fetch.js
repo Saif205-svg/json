a09c87bcf54ffcd7162cecfe3432d52604f3ee8b
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-03-02 15:45:26 +0600

    post method

--- fetch.js
+++ fetch.js
@@ -1,1 +1,23 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1').then(res=>res.json()).then(mydata=>console.log(mydata));
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //promise of response
+    .then(res=>res.json())
+    //promise of json data
+    .then(mydata=>console.log(mydata));
+};
+
+const postData=() => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then((post)=>post.json())
+    .then((data)=> {
+        console.log(data);
+        displayPost(data);
+    })
+};
+
+const displayPost = (posts)=>{
+    posts.forEach((posts) =>{
+        console.log(posts);
+    })
+}
